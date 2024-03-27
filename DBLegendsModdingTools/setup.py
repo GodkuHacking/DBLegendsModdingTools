7b07efd81f35f1550c49b0ca4f62fbbc6e4251a2
Godku <138173273+GodkuHacking@users.noreply.github.com>
2024-03-27 17:55:01 -0400

    preps

---
+++ DBLegendsModdingTools/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DBLegendsModdingTools',
+    version='1.0.0',
+    author='GodkuDBL',
+    description='Tools for modding Dragon Ball Legends',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/GodkuHacking/DBLegendsModdingTools',
+    packages=find_packages(),
+    install_requires=['requests'],
+    package_data={
+        'DBLegendsModdingTools': ['src/*.py'],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
